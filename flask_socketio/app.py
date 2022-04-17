f58f16204ab05de3b4712886dc45b57f02b6ec61
Wolfram-180 <sergei.ulvis@gmail.com>
2022-04-17 14:59:04 +0300

    events added

--- flask_socketio/app.py
+++ flask_socketio/app.py
@@ -39,16 +39,48 @@ def disconnect():
     print('client disconnected')
 
 
+@socketio.on('message')
+def handle_message(data):
+    print('received message: ' + data)
+
+
+@socketio.on('json')
+def handle_json(json):
+    print('received json: ' + str(json))    
+
+
+@socketio.on('event1')
+def handle_event1(json):
+    print('received json event1: ' + str(json))
+
+
+@socketio.on('event2')
+def handle_event2(arg1, arg2, arg3):
+    print('received args event2: ' + arg1 + arg2 + arg3)
+
+
+@socketio.event
+def event3(arg1, arg2, arg3):
+    print('received args event3: ' + arg1 + arg2 + arg3)
+
+
+@socketio.on('test_nmspc_event', namespace='/test')
+def test_nmspc_event(json):
+    print('received json test_nmspc_event: ' + str(json))
+
+
+def event4_handler(data):
+    print('event4_handler received data: ' + str(data))
+
+socketio.on_event('event4', event4_handler, namespace='/test')
+
+
+
 @socketio.on('join')
 def join_group_room(room_id, client_id):
     join_room(room_id)
     print('client ', client_id, ' joined to ', room_id)
     emit('joined', client_id)
-
-
-@socketio.on('message')
-def message_room(data):
-    print(data)
 
 
 if __name__ == '__main__':
